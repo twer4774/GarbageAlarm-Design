bba15e6943b9199342a2b755111d231c33373805
Jo Wonik <twer4774@gmail.com>
2018-02-13 14:17:11 +0900

    클린하우스표시를 위한 프로토콜추가

--- Protocol/Protocol.swift
+++ Protocol/Protocol.swift
@@ -50,3 +50,27 @@ extension JsonParserProtocol{
     }
 }
 
+protocol CleanHouseAPIProtocol{
+    var currentElement: String {get set}
+    var passData: Bool {get set}
+    
+    var parseAddress: Bool {get set}
+    var parseDong: Bool {get set}
+    var parseLocation: Bool {get set}
+    var parseMapx: Bool {get set}
+    var parseMapy: Bool {get set}
+    
+    var address: String {get set}
+    var dong: String {get set}
+    var location: String {get set}
+    //    var mapx: Double {get set}
+    //    var mapy: Double {get set}
+    
+    var mapx: String {get set}
+    var mapy: String {get set}
+}
+
+protocol CleanHouseAPIParser{
+    
+}
+
