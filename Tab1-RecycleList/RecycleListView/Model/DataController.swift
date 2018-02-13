588351dae57cb82cb736788f1d01806b077ec1cc
Jo Wonik <twer4774@gmail.com>
2018-02-13 14:20:35 +0900

    알람 에러처리

--- Tab1-RecycleList/RecycleListView/Model/DataController.swift
+++ Tab1-RecycleList/RecycleListView/Model/DataController.swift
@@ -17,7 +17,7 @@ class DataController{
         let now = Date()
         let comps = cal.dateComponents([.weekday], from: now)
         
-        var calDay = comps.weekday! //숫자로 표기됨
+        var calDay = (comps.weekday!-1) //숫자로 표기됨
         
         return calDay
     }
@@ -29,19 +29,19 @@ class DataController{
         
         //숫자를 한글로 변환하기 위한 스위치문
         switch calDay(){
-        case 1:
+        case 0:
             day = "일요일"
-        case 2:
+        case 1:
             day = "월요일"
-        case 3:
+        case 2:
             day = "화요일"
-        case 4:
+        case 3:
             day = "수요일"
-        case 5:
+        case 4:
             day = "목요일"
-        case 6:
+        case 5:
             day = "금요일"
-        case 7:
+        case 6:
             day = "토요일"
         default:
             print("요일을 찾을 수 없습니다.")
@@ -53,6 +53,41 @@ class DataController{
     func cardIndex(index: Int) -> Int{
         var cardNum = index
         return cardNum
+    }
+    
+    //JSON에서 저장한 commnet
+    func getComment() -> String{
+        var comment = ""
+        
+        var dayModel = DayModel()
+        dayModel.getJSON()
+        
+        for i in dayModel.list{
+            if(getDay() == i.day){
+                comment = i.comment!
+            }
+        }
+        
+        print(comment)
+        return comment
+    }
+    
+    
+    //요일에 맞추어 메모 푸시로 보내기
+    func getMemo() -> String?{
+        
+        let ud = UserDefaults.standard
+        
+        var memo = ud.string(forKey: "memo\(calDay())")
+        
+        
+//       memo = ud.string(forKey: "memo\(calDay())")
+        if memo == nil {
+            memo = "메모가 없습니다."
+        }
+        
+        
+        return memo
     }
 }
 
